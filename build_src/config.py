3ed9df454f6d9146c5350df20ba4a7f2744837aa
Ursula C Johnson <ursulacjohnson@Ursulas-MBP-2.attlocal.net>
2020-08-18 17:03:00 -0500

    committing again

---
+++ build_src/config.py
@@ -0,0 +1,19 @@
+# Write in your username here in the format email/token
+user = "cedilloj@adobe.com/token"
+
+# Add in the base path for your repo here
+base_path= "knowledge-base/src"
+
+# Write out your article ids here as a comma separated list
+articles = [360041456192, 360040757112, 360047251111]
+
+
+
+
+
+
+
+
+
+
+
