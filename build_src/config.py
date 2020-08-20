ab20a0c63f93f76d9262e2b155ba9e8bd68f37be
Ursula <ursulacedillojohnson@gmail.com>
2020-08-20 17:11:00 -0500

    committing changes

--- build_src/config.py
+++ build_src/config.py
@@ -12,7 +12,12 @@ base_path= "src"
 converter.get_published_articles(user, base_path, articles)
 
 
+# Copyright 2020 Adobe
+# All Rights Reserved.
 
+# NOTICE: Adobe permits you to use, modify, and distribute this file in
+# accordance with the terms of the Adobe license agreement accompanying
+# it.
 
 
 
